e6fda623a5fddce0aec46524c8c61afa13057fa1
Andrii Popov <andrii.popov@gen.tech>
2023-05-17 15:50:10 +0300

    add comment

--- code/src/Service/AchievementProvider.php
+++ code/src/Service/AchievementProvider.php
@@ -6,6 +6,16 @@ namespace App\Service;
 
 use App\ValueObject\Achievement;
 
+/**
+ * Необхідно відрефакторити цей клас так, щоб ми могли в юніт тесті перевірити час створення Achievement з точністю до секунди.
+ * Наразі це неможливо за рахунок виклику sleep, що моделює т.з. нестабільний тест.
+ *
+ * Дуже часто саме такі тести час від часу не проходять через те, що при спробі порівняти дату,
+ * яка генерується в самому методі як поточна з поточною в юніт тесті буде
+ * розбіжність на рівні мілісекунд.
+ *
+ * В нашому випадку я для зручності просто додав паузу в 2 секунди щоб це було очевидно.
+ */
 class AchievementProvider implements AchievementProviderInterface
 {
     /** {@inheritDoc} */
@@ -13,6 +23,11 @@ class AchievementProvider implements AchievementProviderInterface
     {
         $firstKey = array_key_first($userWeightLogs);
         $lastKey = array_key_last($userWeightLogs);
+
+        /**
+         * Це додано для моделювання нестабільного тесту. Не прибирати
+         */
+        sleep(2);
 
         $weightDiff = $userWeightLogs[$lastKey]->getWeightGrammes() - $userWeightLogs[$firstKey]->getWeightGrammes();
 
---
+++ code/tests/Unit/Achievement/AchievementProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Achievement;
+
+use PHPUnit\Framework\TestCase;
+
+class AchievementProviderTest extends TestCase
+{
+    // todo
+}
